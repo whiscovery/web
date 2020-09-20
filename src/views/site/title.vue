d49e75da025860fb60c50a07ef4631f28f820795
whiscovery <whiscovery@gmail.com>
2020-09-21 00:14:42 +0900

    에러핸들링

--- src/views/site/title.vue
+++ src/views/site/title.vue
@@ -4,9 +4,15 @@
        <v-btn icon @click="openDialog"><v-icon>mdi-grease-pencil</v-icon></v-btn>
        <v-dialog v-model="dialog" max-width="400">
          <v-card>
-           <v-card-title>제목 수정</v-card-title>
+           <v-card-title>
+             제목 수정
+             <v-spacer />
+             <v-btn icon @click="save"><v-icon>mdi-content-save</v-icon></v-btn>
+             <v-btn icon @click="dialog=false"><v-icon>mdi-close</v-icon></v-btn>
+
+           </v-card-title>
            <v-card-text>
-             <v-text-field v-model="text" outlined label="제목" @keypress.enter="save" />
+             <v-text-field v-model="text" outlined label="제목" @keypress.enter="save" hide-details/>
            </v-card-text>
          </v-card>
        </v-dialog>
@@ -27,8 +33,14 @@ export default {
     openDialog () {
       this.dialog = true
     },
-    save () {
-      this.$firebase.database().ref().child('site').update({ title: this.text })
+    async save () {
+      try {
+        this.$firebase.database().ref().child('site').update({ title: this.text })
+      } catch (e) {
+        console.log(e.message)
+      } finally {
+        this.dialog = false
+      }
     }
   }
 }
