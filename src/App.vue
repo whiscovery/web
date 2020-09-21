0dd172618cba636ec5e3d98ba85b10853aceaae2
whiscovery <whiscovery@gmail.com>
2020-09-21 16:47:24 +0900

    21 하위메뉴 만들기

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-     <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-     <site-title :title="site.title"></site-title>
-
-     <v-spacer></v-spacer>
-     <v-btn icon @click="save"><v-icon>mdi-check</v-icon></v-btn>
-     <v-btn icon @click="read"><v-icon>mdi-book</v-icon></v-btn>
-     <v-btn icon @click="readOne"><v-icon>mdi-note</v-icon></v-btn>
-
+    <v-app-bar app color="primary" dark>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
+      <site-title :title="site.title"></site-title>
+      <v-spacer/>
     </v-app-bar>
-     <v-navigation-drawer app v-model="drawer">
+    <v-navigation-drawer app v-model="drawer" width="400">
       <site-menu :items="site.menu"></site-menu>
-     </v-navigation-drawer>
-
-    <v-main>
-      <router-view></router-view>
-    </v-main>
-
-   <site-footer :footer="site.footer"></site-footer>
+    </v-navigation-drawer>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <site-footer :footer="site.footer"></site-footer>
   </v-app>
 </template>
 
@@ -38,9 +27,35 @@ export default {
     return {
       drawer: false,
       site: {
-        title: 'Whiscovery',
-        menu: [],
-        footer: 'made by Whiscovery'
+        menu: [
+          {
+            title: 'home',
+            icon: 'mdi-home',
+            subItems: [
+              {
+                title: 'Dashboard',
+                to: '/'
+              },
+              {
+                title: 'About',
+                to: '/about'
+              }
+            ]
+          },
+          {
+            title: 'about',
+            active: true,
+            icon: 'mdi-account',
+            subItems: [
+              {
+                title: 'xxx',
+                to: '/xxx'
+              }
+            ]
+          }
+        ],
+        title: '나의 타이틀입니다',
+        footer: '푸터입니다'
       }
     }
   },
@@ -49,31 +64,16 @@ export default {
   },
   methods: {
     subscribe () {
-      this.$firebase.database().ref().child('site').on('value', sn => {
+      this.$firebase.database().ref().child('site').on('value', (sn) => {
         const v = sn.val()
         if (!v) {
           this.$firebase.database().ref().child('site').set(this.site)
+          return
         }
         this.site = v
       }, (e) => {
         console.log(e.message)
       })
-    },
-    save () {
-      this.$firebase.database().ref().child('abcd').set({
-        title: 'abcd',
-        text: 'tttt'
-      })
-    },
-    read () {
-      this.$firebase.database().ref().child('abcd').on('value', sn => {
-        console.log(sn)
-        console.log(sn.val())
-      })
-    },
-    async readOne () {
-      const sn = await this.$firebase.database().ref().child('abcd').once('value')
-      console.log(sn.val())
     }
   }
 }
--- src/views/Input.vue
+++ src/views/Input.vue
@@ -1,13 +1,13 @@
 <template>
-    
+
 </template>
 
 <script>
 export default {
-    data(){
-        return {
-            
-        }
+  data () {
+    return {
+
     }
+  }
 }
 </script>
